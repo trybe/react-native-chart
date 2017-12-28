55a95f53849e44074217d3c0bdc97e2ca2b8de6a
Vincent Tam <vince@trybe.do>
2017-12-28 12:05:02 -0800

    (feat) Update dist files by running npm install and npm run build

--- dist/xAxis.js
+++ dist/xAxis.js
@@ -1,4 +1,7 @@
-'use strict';Object.defineProperty(exports,"__esModule",{value:true});var _jsxFileName='src/xAxis.js';var _createClass=function(){function defineProperties(target,props){for(var i=0;i<props.length;i++){var descriptor=props[i];descriptor.enumerable=descriptor.enumerable||false;descriptor.configurable=true;if("value" in descriptor)descriptor.writable=true;Object.defineProperty(target,descriptor.key,descriptor);}}return function(Constructor,protoProps,staticProps){if(protoProps)defineProperties(Constructor.prototype,protoProps);if(staticProps)defineProperties(Constructor,staticProps);return Constructor;};}();
+'use strict';Object.defineProperty(exports,"__esModule",{value:true});var _jsxFileName='src/xAxis.js';var _createClass=function(){function defineProperties(target,props){for(var i=0;i<props.length;i++){var descriptor=props[i];descriptor.enumerable=descriptor.enumerable||false;descriptor.configurable=true;if("value"in descriptor)descriptor.writable=true;Object.defineProperty(target,descriptor.key,descriptor);}}return function(Constructor,protoProps,staticProps){if(protoProps)defineProperties(Constructor.prototype,protoProps);if(staticProps)defineProperties(Constructor,staticProps);return Constructor;};}();
+
+
+var _propTypes=require('prop-types');var _propTypes2=_interopRequireDefault(_propTypes);
 
 var _react=require('react');var _react2=_interopRequireDefault(_react);
 var _reactNative=require('react-native');function _interopRequireDefault(obj){return obj&&obj.__esModule?obj:{default:obj};}function _classCallCheck(instance,Constructor){if(!(instance instanceof Constructor)){throw new TypeError("Cannot call a class as a function");}}function _possibleConstructorReturn(self,call){if(!self){throw new ReferenceError("this hasn't been initialised - super() hasn't been called");}return call&&(typeof call==="object"||typeof call==="function")?call:self;}function _inherits(subClass,superClass){if(typeof superClass!=="function"&&superClass!==null){throw new TypeError("Super expression must either be null or a function, not "+typeof superClass);}subClass.prototype=Object.create(superClass&&superClass.prototype,{constructor:{value:subClass,enumerable:false,writable:true,configurable:true}});if(superClass)Object.setPrototypeOf?Object.setPrototypeOf(subClass,superClass):subClass.__proto__=superClass;}
@@ -12,11 +15,11 @@ justifyContent:'space-between'},
 
 axisText:{
 flex:1,
-backgroundColor:'transparent'}});var 
+backgroundColor:'transparent'}});var
 
 
 
-XAxis=function(_Component){_inherits(XAxis,_Component);function XAxis(){_classCallCheck(this,XAxis);return _possibleConstructorReturn(this,Object.getPrototypeOf(XAxis).apply(this,arguments));}_createClass(XAxis,[{key:'render',value:function render()
+XAxis=function(_Component){_inherits(XAxis,_Component);function XAxis(){_classCallCheck(this,XAxis);return _possibleConstructorReturn(this,(XAxis.__proto__||Object.getPrototypeOf(XAxis)).apply(this,arguments));}_createClass(XAxis,[{key:'render',value:function render()
 
 
 
@@ -38,9 +41,9 @@ XAxis=function(_Component){_inherits(XAxis,_Component);function XAxis(){_classCa
 var data=this.props.data||[];
 var transform=function transform(d){return d;};
 if(this.props.xAxisTransform&&typeof this.props.xAxisTransform==='function'){
-transform=this.props.xAxisTransform;}
-
-return (
+transform=this.props.xAxisTransform;
+}
+return(
 _react2.default.createElement(_reactNative.View,{
 style:[
 styles.xAxisContainer,
@@ -48,7 +51,7 @@ styles.xAxisContainer,
 borderTopColor:this.props.axisColor,
 borderTopWidth:this.props.axisLineWidth},
 
-this.props.style],__source:{fileName:_jsxFileName,lineNumber:44}},
+this.props.style],__source:{fileName:_jsxFileName,lineNumber:47}},
 
 
 function(){
@@ -56,7 +59,7 @@ if(!_this2.props.showXAxisLabels)return null;
 return data.map(function(d,i){
 var item=transform(d[0]);
 if(typeof item!=='number'&&!item)return null;
-return (
+return(
 _react2.default.createElement(_reactNative.Text,{
 key:i,
 style:[
@@ -64,7 +67,13 @@ styles.axisText,
 {
 textAlign:_this2.props.align,
 color:_this2.props.axisLabelColor,
-fontSize:_this2.props.labelFontSize}],__source:{fileName:_jsxFileName,lineNumber:60}},
+fontSize:_this2.props.labelFontSize}],__source:{fileName:_jsxFileName,lineNumber:63}},
 
 
-item));});}()));}}]);return XAxis;}(_react.Component);XAxis.propTypes={axisColor:_react.PropTypes.any.isRequired,axisLabelColor:_react.PropTypes.any.isRequired,axisLineWidth:_react.PropTypes.number.isRequired,data:_react.PropTypes.arrayOf(_react.PropTypes.array),showXAxisLabels:_react.PropTypes.bool.isRequired,style:_react.PropTypes.any,width:_react.PropTypes.number.isRequired,align:_react.PropTypes.string,labelFontSize:_react.PropTypes.number.isRequired,xAxisTransform:_react.PropTypes.func};XAxis.defaultProps={align:'center'};exports.default=XAxis;
+item));
+
+});
+}()));
+
+
+}}]);return XAxis;}(_react.Component);XAxis.propTypes={axisColor:_propTypes2.default.any.isRequired,axisLabelColor:_propTypes2.default.any.isRequired,axisLineWidth:_propTypes2.default.number.isRequired,data:_propTypes2.default.arrayOf(_propTypes2.default.array),showXAxisLabels:_propTypes2.default.bool.isRequired,style:_propTypes2.default.any,width:_propTypes2.default.number.isRequired,align:_propTypes2.default.string,labelFontSize:_propTypes2.default.number.isRequired,xAxisTransform:_propTypes2.default.func};XAxis.defaultProps={align:'center'};exports.default=XAxis;
